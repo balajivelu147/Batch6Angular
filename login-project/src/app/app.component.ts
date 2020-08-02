a8cc092c48142499522bc482b34f1975136f9e9c
sharath2895 <assharath2895@gmail.com>
2020-08-02 18:58:13 +0530

    Login committed

--- login-project/src/app/app.component.ts
+++ login-project/src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login-project';
+  function login() {
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var cpassword = document.getElementById("cpassword").value;
+    var age = document.getElementById("age").value;
+    var gender = document.getElementById("gender").value;
+
+    if (firstname == "") {
+        alert(" please enter firstname")
+        console(" please enter firstname");
+    }
+    if (email == "") {
+        alert("please enter email id")
+        console("please enter email id");
+    }
+    if (password == "") {
+        alert(" please enter password")
+        console(" please enter password");
+    }
+    if (password == cpassword) {
+        console(" passwords matched");
+        alert("passwords matched")
+    }
+    else {
+        alert("please enter same passwords")
+    }
+    if (firstname == "sharath " && email == "assharath2895@gmail.com" && password == "sharu" && cpassword == "sharu") {
+        console.log("firstname : " + firstname);
+        console.log("lastname : " + lastname);
+        console.log("email : " + email);
+        console.log("password : " + password);
+        console.log("cpassword : " + cpassword);
+        console("sharath account created successfully")
+        alert(" sharath account created successfully")
+    }
+    else {
+        console.log(" wrong credential entered");
+        alert("wrong credentials entered");
+    }
+
+
+}
 }
