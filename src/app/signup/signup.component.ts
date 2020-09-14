770a69b70f3c182484f925aa074f1d209e3535ee
Velupriya <velupriya@thinkbridge.in>
2020-09-14 07:50:00 +0530

    subject

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SampleService } from 'src/shared/services/sample.service';
-import { map, tap, switchMap } from 'rxjs/operators';
+import { map, tap, switchMap, first } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -55,7 +56,94 @@ export class SignupComponent implements OnInit {
       //    value + 'Engineer'
       //   ))
       // .subscribe();
-  }
+
+      // this.sampleService.setSubject('balaji');
+     this.sampleService.sampleSubject$.pipe(
+       tap((value) => console.log('subject'+value) )
+     ).subscribe();
+
+
+     this.sampleService.setBehSubject(10);
+     this.sampleService.getBehSubject().subscribe(x => console.log('behValue'+x.toString()))
+ 
+     this.sampleService.setBehSubject(100);
+     this.sampleService.getBehSubject().subscribe(x => console.log('behValue'+x.toString()))
+ 
+     this.sampleService.setBehSubject(1000);
+     this.sampleService.getBehSubject().subscribe(x => console.log('behValue'+x.toString()))
+ 
+     this.sampleService.setReplaySubject(1);
+     this.sampleService.setReplaySubject(2);
+     this.sampleService.setReplaySubject(3);
+     this.sampleService.sampleReplaySubject.pipe().subscribe(x => console.log('replayValue'+x.toString())).unsubscribe()
+
+     this.sampleService.setReplaySubject(4);
+     this.sampleService.setReplaySubject(5);
+     this.sampleService.sampleReplaySubject.pipe().subscribe(x => console.log('replayValue'+x.toString())).unsubscribe()
+
+     this.sampleService.setReplaySubject(6);
+     this.sampleService.setReplaySubject(7);
+     this.sampleService.setReplaySubject(8);
+     this.sampleService.sampleReplaySubject.pipe().subscribe(x => console.log('replayValue'+x.toString())).unsubscribe()
+
+     this.sampleService.setReplaySubject(9);
+     this.sampleService.setReplaySubject(10);
+     this.sampleService.setReplaySubject(11);
+
+
+
+     this.sampleService.setAsyncSubject(1);
+     this.sampleService.setAsyncSubject(2);
+     this.sampleService.setAsyncSubject(3);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+     this.sampleService.completeAsyncSubject();
+
+     this.sampleService.setAsyncSubject(4);
+     this.sampleService.setAsyncSubject(5);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+
+     this.sampleService.setAsyncSubject(6);
+     this.sampleService.setAsyncSubject(7);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+
+     this.sampleService.setAsyncSubject(8);
+     this.sampleService.setAsyncSubject(9);
+     this.sampleService.setAsyncSubject(10);
+     this.sampleService.setAsyncSubject(11);
+     this.sampleService.setAsyncSubject(12);
+     this.sampleService.getAsyncSubject().subscribe(console.log);
+
+     this.sampleService.setAsyncSubject(13);
+     this.sampleService.completeAsyncSubject();
+
+     this.sampleService.setAsyncSubject(14);
+
+
+
+
+//      const sub = new ReplaySubject(3);
+
+// sub.next(1);
+// sub.next(2);
+// sub.subscribe(() => console.log("one")); 
+// sub.subscribe(console.log); // OUTPUT => 1,2
+// sub.next(3); // OUTPUT => 3
+// sub.next(4); // OUTPUT => 4
+// sub.subscribe(() => console.log("two")); 
+
+// sub.subscribe(console.log); // OUTPUT => 2,3,4 (log of last 3 values from new subscriber)
+// sub.next(5);
+
+
+      // this.sampleService.getreplaySubject().subscribe(x => console.log('replayValue'+x.toString()))
+ 
+   
+   
+   
+    }
 
 
 
--- src/shared/services/sample.service.ts
+++ src/shared/services/sample.service.ts
@@ -16,20 +16,54 @@ export class SampleService {
 
   public sampleObservable$: Observable<string> = of('balaji');
 
-  public sampleSubject = new Subject();
-  public sampleSubject$ = this.sampleSubject as Observable<any>;
+  public sampleSubject = new Subject<string>();
+   public sampleSubject$: Observable<string> = this.sampleSubject.asObservable();
   public sampleBehaviourSubject = new BehaviorSubject(5);
-  public sampleReplaySubject = new ReplaySubject(6);
-  public sampleAsyncSubject = new AsyncSubject();
+  public sampleBehaviourSubject$: Observable<number> = this.sampleBehaviourSubject.asObservable();
+  public sampleReplaySubject = new ReplaySubject<number>(3);
+  // public sampleReplaySubject$: Observable<number> = this.sampleReplaySubject.asObservable();
+  public sampleAsyncSubject = new AsyncSubject<number>();
 
 
-  get Subject() {
+
+  getAsyncSubject(): Observable<number> {
+    return this.sampleAsyncSubject;
+  }
+
+  setAsyncSubject(value) {
+    this.sampleAsyncSubject.next(value);
+  }
+
+  completeAsyncSubject() {
+    this.sampleAsyncSubject.complete();
+  }
+
+  getSubject(): Observable<string> {
     return this.sampleSubject$;
   }
 
-  set Subject(value) {
+  setSubject(value) {
     this.sampleSubject.next(value);
   }
+
+  
+  getBehSubject(): Observable<number> {
+    return this.sampleBehaviourSubject$;
+  }
+
+  setBehSubject(value) {
+    this.sampleBehaviourSubject.next(value);
+  }
+
+
+  // getreplaySubject(): Observable<number> {
+  //   return this.sampleReplaySubject$;
+  // }
+
+  setReplaySubject(value) {
+    this.sampleReplaySubject.next(value);
+  }
+
 
 
   sampleAdder(a, b) {
